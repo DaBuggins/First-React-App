4965cb541ba329860f0a073f63222fa1ad32c7a5
DaBuggins <willenrowe@gmail.com>
2023-05-17 19:19:01 +0100

    first vite react project

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+import Chicken from './Chicken';
+import Greeter from './Greeter';
+
+function App() {
+  return (<div>
+<Greeter />
+  </div>);
+}
+
+export default App;
---
+++ src/Chicken.jsx
@@ -0,0 +1,6 @@
+import "./Chicken.css";
+function Chicken() {
+    return <p className="Chicken">Cluck cluck!</p>;
+}
+
+export default Chicken;
